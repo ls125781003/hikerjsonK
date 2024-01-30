af7b70b52cab2ad68e1ae695c8a3ea081f66d0af
xiaoliu <125781003@qq.com>
2024-01-30 21:29:05 +0800

    Add files via upload

--- 顺搜.js
+++ 顺搜.js
@@ -1,30 +1,35 @@
-function 顺搜(arr) {
+function 顺搜(arr, height) {
 	if (MY_PAGE == 1) {
-		putVar('小程序名', MY_RULE.title);
+		var 程序名=MY_RULE.title;
+		putVar('小程序名', 程序名);
+		putVar(程序名+'顺搜高度', JSON.stringify(height));
 		var 本地 =getPath('hiker://files/rules/dzHouse/html/顺搜.html');
 		if (fileExist(本地) == false) {
-			var 远程x5 = request('https://ghproxy.net/https://raw.githubusercontent.com/ls125781003/hikerjsonK/main/顺搜.html');
+			var 远程x5 = request('https://ghproxy.net/https://raw.githubusercontent.com/ls125781003/hikerdbw/master/顺搜.html');
 			if (远程x5.indexOf("search_bg") > 0) {
 				writeFile(本地, 远程x5);
 			} else {
 				confirm({
 					title: '❌错误提示',
-					content: '顺搜导入出错\n请到道长仓库手动导入顺搜.html'
+					content: '顺搜导入出错'
 				})
 			}
 		}
 		if (!getVar('X5加载')) {
 			x5_height = 0
 		} else {
-			x5_height = 'video'
+			x5_height = getVar(程序名+'顺搜高度', 'video')
 		}
 		arr.push({
 			desc: 'list&&' + x5_height,
 			url: 本地,
 			col_type: 'x5_webview_single',
 			extra: {
-				ua: MOBILE_UA
+				ua: MOBILE_UA,
+				autoPlay: true,
+				imgLongClick: false
 			}
 		})
+		return arr
 	}
 }
